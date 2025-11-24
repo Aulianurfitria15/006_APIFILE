fe97e8daa03aed0ab32d714bd9b5d08a69d04e58
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-24 12:11:03 +0700

    menambahkan isi ke file komikService.js

---
+++ services/komikService.js
@@ -0,0 +1,77 @@
+async function createKomik(database, komikData) {
+  const { title, description, author, imageType, imageName, imageData } = komikData;
+
+  if (!title || !description || !author) {
+    throw new Error('Title, description, dan author wajib diisi');
+  }
+
+  const newKomik = await database.Komik.create({
+    title,
+    description,
+    author,
+    imageType: imageType || null,
+    imageName: imageName || null,
+    imageData: imageData || null,
+  });
+
+  return newKomik;
+}
+
+// Fungsi untuk mendapatkan semua komik
+async function getAllKomik(database) {
+  const komiks = await database.Komik.findAll();
+
+  return komiks.map(k => {
+    if (k.imageData) {
+      k.imageData = k.imageData.toString('base64');
+    }
+    return k;
+  });
+}
+
+// Fungsi untuk mendapatkan komik berdasarkan ID
+async function getKomikById(database, id) {
+  const komik = await database.Komik.findByPk(id);
+
+  if (!komik) throw new Error('Komik tidak ditemukan');
+
+  if (komik.imageData) {
+    komik.imageData = komik.imageData.toString('base64');
+  }
+
+  return komik;
+}
+
+// Kelanjutan dari komikService.js
+
+// Fungsi untuk memperbarui data komik
+async function updateKomik(database, id, komikData) {
+  const komik = await database.Komik.findByPk(id);
+  
+  if (!komik) {
+    throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+  }
+
+  await komik.update(komikData);
+  return komik;
+}
+
+// Fungsi untuk menghapus komik
+async function deleteKomik(database, id) {
+  const komik = await database.Komik.findByPk(id);
+  
+  if (!komik) {
+    throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+  }
+
+  await komik.destroy();
+  return { message: `Komik dengan ID ${id} berhasil dihapus` };
+}
+
+module.exports = {
+  createKomik,
+  getAllKomik,
+  getKomikById,
+  updateKomik,
+  deleteKomik,
+};
